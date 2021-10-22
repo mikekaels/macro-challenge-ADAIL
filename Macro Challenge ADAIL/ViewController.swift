1382df59ad2fa09d2d47ea984bc1ec4c283b04cc
kaelsihombing <sntmcl1@gmail.com>
2021-10-22 21:07:40 +0700

    Initial Commit

---
+++ Macro Challenge ADAIL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Macro Challenge ADAIL
+//
+//  Created by Santo Michael Sihombing on 22/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
